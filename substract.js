186d53ee001181e61e6f77935c446492c237cb24
Martin Popovic <martinpopovicfit@gmail.com>
2019-06-08 13:19:38 +0200

    extra feature in substract function

--- substract.js
+++ substract.js
@@ -1,4 +1,16 @@
-// subtract.js
+<<<<<<< Updated upstream
+// subtraction of two numbers
 function subtract(a, b) {
 	// TODO return
+=======
+// subtract.js
+// return subtraction of two numbers
+function subtract(a, b, bMINUSa) {
+	if (bMINUSa === true) {
+			return b - a;
+	}
+	else {
+			return a - b;
+	}
+>>>>>>> Stashed changes
 };
